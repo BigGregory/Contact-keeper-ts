055f618be363dc90553a7f4576c546134b635179
Hryhorii Tereshchenko <hryhoriit@ext.inditex.com>
2023-04-17 14:21:57 +0300

    added initial context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,21 +4,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import ContactState from './context/contact/ContactState';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Navbar title="Contact Keeper" icon="fas fa-regular fa-address-card" />
-        <div className="container">
-          <Routes>
-            <Route path="/" Component={Home} />
-            <Route path="/about" Component={About} />
-          </Routes>
+    <ContactState>
+      <Router>
+        <div className="App">
+          <Navbar
+            title="Contact Keeper"
+            icon="fas fa-regular fa-address-card"
+          />
+          <div className="container">
+            <Routes>
+              <Route path="/" Component={Home} />
+              <Route path="/about" Component={About} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ContactState>
   );
 };
 
